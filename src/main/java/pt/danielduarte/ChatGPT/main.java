3ae306852e852cd23a4bf2b2ae7294e628f7e62d
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-03-22 15:29:46 +0000

    First Commit

---
+++ src/main/java/pt/danielduarte/ChatGPT/main.java
@@ -0,0 +1,9 @@
+package pt.danielduarte.ChatGPT;
+
+public class main {
+    private static final String TOKEN ="";
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pt/danielduarte/ChatGPT/model/request/OpenAIRequest.java
@@ -0,0 +1,8 @@
+package pt.danielduarte.ChatGPT.model.request;
+
+public class OpenAIRequest {
+    private String model;
+    private String prompt;
+    private Integer maxTokens;
+    private Float temperature;
+}
