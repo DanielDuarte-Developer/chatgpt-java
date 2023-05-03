2ff8deaf7b3a7c09f75478f915885f88aeb31ce0
DanielDuarte-Developer <daniel.duarte@cmas-systems.com>
2023-05-03 15:53:25 +0100

    Implement Chat and Make ChatGpt Better

--- src/main/java/pt/danielduarte/ChatGPT/Main.java
+++ src/main/java/pt/danielduarte/ChatGPT/Main.java
@@ -1,40 +1,36 @@
 package pt.danielduarte.ChatGPT;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import pt.danielduarte.ChatGPT.client.OpenAiCompletionsClient;
 import pt.danielduarte.ChatGPT.client.OpenAiResponseHandler;
 import pt.danielduarte.ChatGPT.mapper.OpenAiRequesToJsonSting;
-import pt.danielduarte.ChatGPT.model.request.OpenAIRequest;
+import pt.danielduarte.ChatGPT.model.request.modelo.Message;
+import pt.danielduarte.ChatGPT.service.OpenAiChatService;
 import pt.danielduarte.ChatGPT.model.request.factory.OpenAiRequestFactory;
 import pt.danielduarte.ChatGPT.model.request.modelo.Chat;
-import pt.danielduarte.ChatGPT.model.request.response.OpenAiResponse;
-import pt.danielduarte.ChatGPT.model.request.response.OpenAiResponseChoice;
 import pt.danielduarte.ChatGPT.util.ConfigParser;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     private static final String TOKEN ="";
 
     public static void main(String[] args) throws IOException {
-
+       final Scanner sc = new Scanner(System.in);
         Map<String,String> config = ConfigParser.parse("application.properties");
         String greeting = config.get("greeting");
         String token = config.get("token");
-        System.out.println(greeting);
-
+        Chat chat = new Chat();
         OpenAiRequestFactory openAiRequestFactory = new OpenAiRequestFactory();
-        Chat chat = new Chat(greeting);
-        OpenAIRequest req = openAiRequestFactory.createChatOpenAiRequest(chat);
+        OpenAiCompletionsClient client = new OpenAiCompletionsClient(token,new OpenAiRequesToJsonSting(),new OpenAiResponseHandler(new ObjectMapper()));
 
-        OpenAiRequesToJsonSting mapper = new OpenAiRequesToJsonSting();
-        OpenAiResponseHandler responseHandler = new OpenAiResponseHandler(new ObjectMapper());
+        final OpenAiChatService service = new OpenAiChatService(openAiRequestFactory,client);
 
-        OpenAiCompletionsClient client = new OpenAiCompletionsClient(token,mapper,responseHandler);
-        OpenAiResponse response = client.post(req);
-
-       response.getChoices().stream().map(OpenAiResponseChoice::getText).forEach(System.out::println);
+        while(true){
+            System.out.println("Person: ");
+            service.sendMessage(chat,sc.nextLine()).forEach(message -> System.out.printf("%s: %s\n", message.getSubject(), message.getContent()));
+        }
     }
 }
--- src/main/java/pt/danielduarte/ChatGPT/model/request/modelo/Chat.java
+++ src/main/java/pt/danielduarte/ChatGPT/model/request/modelo/Chat.java
@@ -4,17 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chat {
-    private final String prompt;
     private final List<Message> messages;
 
-    public Chat(String prompt) {
-        this.prompt = prompt;
+    public Chat() {
         this.messages = new ArrayList<>();
     }
 
-    public String getPrompt() {
-        return prompt;
-    }
     public List<Message> getMessages() {
         return messages;
     }
